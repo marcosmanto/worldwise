72f8604c6f620f4636a8d795b73adf6a8793f1d5
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-25 10:37:18 -0300

    create nested routes on app, add new components and styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
+import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
+import Login from './pages/Login'
+import City from './components/City'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<City />} />
+          <Route path="cities" element={<City />} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-import PageNav from '../components/PageNav'
-
-function PageNotFound() {
+export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      Page not found 🙁
+      <h1>Page not found 😢</h1>
     </div>
-  )
+  );
 }
-
-export default PageNotFound
