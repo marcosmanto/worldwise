6b0864f7c7ea5f1bf4cdfdacf1de498cab19faee
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-25 12:30:36 -0300

    implement flag emoji in `CityItem` component. The `flagemojiToPNG` is used because windows and some linux systems not render country codes as emojis like Apple systems.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,5 +1,21 @@
-function CityItem() {
-  return <li>City</li>
+import styles from './CityItem.module.css'
+
+const flagemojiToPNG = flag => {
+  var countryCode = Array.from(flag, codeUnit => codeUnit.codePointAt())
+    .map(char => String.fromCharCode(char - 127397).toLowerCase())
+    .join('')
+  return <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+}
+
+function CityItem({ city }) {
+  const { cityName, emoji, date } = city
+
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+      <h3 className={styles.name}>{cityName}</h3>
+    </li>
+  )
 }
 
 export default CityItem
