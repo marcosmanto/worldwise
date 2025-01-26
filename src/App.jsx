deaaca6a6b6ee18e474aea9700004f409ff03cda
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-26 19:39:47 -0300

    add more components.  `Form` component appears when click on `Map`

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Product from './pages/Product'
 import Pricing from './pages/Pricing'
 import Homepage from './pages/Homepage'
@@ -6,7 +8,9 @@ import AppLayout from './pages/AppLayout'
 import PageNotFound from './pages/PageNotFound'
 import Login from './pages/Login'
 import CityList from './components/CityList'
-import { useEffect, useState } from 'react'
+import City from './components/City'
+import CountryList from './components/CountryList'
+import Form from './components/Form'
 
 const BASE_URL = 'http://localhost:8000'
 
@@ -40,8 +44,9 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path="countries" element={<p>Countries</p>} />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading} />} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/CountryItem.jsx
+++ src/components/CountryItem.jsx
@@ -1,12 +1,14 @@
-import styles from "./CountryItem.module.css";
+import styles from './CountryItem.module.css'
+
+import { flagemojiToPNG } from './Utils'
 
 function CountryItem({ country }) {
   return (
     <li className={styles.countryItem}>
-      <span>{country.emoji}</span>
+      <span>{flagemojiToPNG(country.emoji)}</span>
       <span>{country.country}</span>
     </li>
-  );
+  )
 }
 
-export default CountryItem;
+export default CountryItem
