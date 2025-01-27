6ad16342aa0c857b0254e9fa4888f9ba7ea18701
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-27 19:47:51 -0300

    Refactor app to use a provider (`CitiesProvider`) for cities data

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import CountryItem from './CountryItem'
 import styles from './CountryList.module.css'
 import Spinner from './Spinner'
 import Message from './Message'
+import { useCities } from '../providers/CitiesProvider'
 
-function CountryList({ cities, isLoading }) {
+function CountryList() {
+  const { cities, isLoading } = useCities()
+
   if (isLoading) return <Spinner />
   if (!cities.length) return <Message message="Add your first city by clicking on a city on the map" />
 
