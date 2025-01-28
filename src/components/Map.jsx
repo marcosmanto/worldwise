452f96fae8d41c166f734b1ed9d427bdabd35465
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-28 00:53:19 -0300

    add leaflet and `mapPosition` state in Map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,17 +1,26 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { useState } from 'react'
 function Map() {
   const navigate = useNavigate()
+  const [mapPosition, setMapPosition] = useState([51.477806, -0.001472])
   const [searchParams, setSearchParams] = useSearchParams()
   const lat = searchParams.get('lat')
   const lng = searchParams.get('lng')
 
+  // openstreetmaps tile provider
+  // https://wiki.openstreetmap.org/wiki/Raster_tile_providers
   return (
     <div className={styles.mapContainer} onClick={() => navigate('form')}>
-      <h1>Map</h1>
-      <h2>
-        Position: {lat}, {lng}
-      </h2>
+      <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' url="https://a.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
