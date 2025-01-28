42c4d72b86269b4369a65dbd10ba5986eaaccc8c
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-28 00:00:49 -0300

    Add `currentCity` state. This highligth the selected city in `CityItem`

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,63 +1,65 @@
-import { useParams, useSearchParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import styles from './City.module.css'
-import { formatDate } from './Utils'
+import { formatDate, flagemojiToPNG } from './Utils'
+import { useCities } from '../providers/CitiesProvider'
+import { useEffect } from 'react'
+import Spinner from './Spinner'
+import ButtonBack from './ButtonBack'
 
 function City() {
   const { id } = useParams()
-  const [searchParams, setSearchParams] = useSearchParams()
-  const lat = searchParams.get('lat')
-  const lng = searchParams.get('lng')
+  const { getCity, currentCity, isLoading } = useCities()
 
-  // TEMP DATA
-  const currentCity = {
-    cityName: 'Lisbon',
-    emoji: '🇵🇹',
-    date: '2027-10-31T15:59:59.138Z',
-    notes: 'My favorite city so far!'
-  }
+  // const [searchParams, setSearchParams] = useSearchParams()
+  // const lat = searchParams.get('lat')
+  // const lng = searchParams.get('lng')
+
+  useEffect(
+    function () {
+      getCity(id)
+    },
+    [id]
+  )
 
   const { cityName, emoji, date, notes } = currentCity
 
+  if (isLoading) return <Spinner />
+
+  if (cityName === undefined) return
+
   return (
-    <>
-      <h1>City {id}</h1>
-      <h2>
-        Pos: {lat}, {lng}
-      </h2>
-    </>
+    <div className={styles.city}>
+      <div className={styles.row}>
+        <h6>City name</h6>
+        <h3>
+          <span>{flagemojiToPNG(emoji)}</span> {cityName}
+        </h3>
+      </div>
+
+      <div className={styles.row}>
+        <h6>You went to {cityName} on</h6>
+        <p>{formatDate(date || null)}</p>
+      </div>
+
+      {notes && (
+        <div className={styles.row}>
+          <h6>Your notes</h6>
+          <p>{notes}</p>
+        </div>
+      )}
+
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a href={`https://en.wikipedia.org/wiki/${cityName}`} target="_blank" rel="noreferrer">
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+
+      <div>
+        <ButtonBack />
+      </div>
+    </div>
   )
-
-  // return (
-  //   <div className={styles.city}>
-  //     <div className={styles.row}>
-  //       <h6>City name</h6>
-  //       <h3>
-  //         <span>{emoji}</span> {cityName}
-  //       </h3>
-  //     </div>
-
-  //     <div className={styles.row}>
-  //       <h6>You went to {cityName} on</h6>
-  //       <p>{formatDate(date || null)}</p>
-  //     </div>
-
-  //     {notes && (
-  //       <div className={styles.row}>
-  //         <h6>Your notes</h6>
-  //         <p>{notes}</p>
-  //       </div>
-  //     )}
-
-  //     <div className={styles.row}>
-  //       <h6>Learn more</h6>
-  //       <a href={`https://en.wikipedia.org/wiki/${cityName}`} target="_blank" rel="noreferrer">
-  //         Check out {cityName} on Wikipedia &rarr;
-  //       </a>
-  //     </div>
-
-  //     <div>{/* <ButtonBack /> */}</div>
-  //   </div>
-  // )
 }
 
 export default City
--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -2,13 +2,16 @@ import { Link } from 'react-router-dom'
 import styles from './CityItem.module.css'
 
 import { flagemojiToPNG, formatDate } from './Utils'
+import { useCities } from '../providers/CitiesProvider'
 
 function CityItem({ city }) {
+  const { currentCity, setCurrentCity } = useCities()
   const { cityName, emoji, date, id, position } = city
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+      {/* onClick set currentCity to empty object to avoid glitch of showing previous selected city */}
+      <Link onClick={() => setCurrentCity({})} className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ''}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
