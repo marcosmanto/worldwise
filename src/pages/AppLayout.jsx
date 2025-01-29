fe6ac2a8b6b1ff953484faba7d35a7f86e44a613
Marcos Mantovani <mantovani.marcos@gmail.com>
2025-01-29 18:43:18 -0300

    implemet fake authentication

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,14 +1,31 @@
+import { useEffect } from 'react'
 import Map from '../components/Map'
 import Sidebar from '../components/Sidebar'
+import User from '../components/User'
+import { useAuth } from '../providers/FakeAuthProvider'
 import styles from './AppLayout.module.css'
+import { useNavigate } from 'react-router-dom'
 
 function AppLayout() {
-  return (
+  const { isAuthenticated } = useAuth()
+  const navigate = useNavigate()
+
+  console.log(isAuthenticated)
+
+  useEffect(
+    function () {
+      if (!isAuthenticated) navigate('/login')
+    },
+    [isAuthenticated]
+  )
+
+  return isAuthenticated ? (
     <div className={styles.app}>
+      <User />
       <Sidebar />
       <Map />
     </div>
-  )
+  ) : null
 }
 
 export default AppLayout
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,37 @@
+import { useNavigate } from 'react-router-dom'
 import PageNav from '../components/PageNav'
+import { useAuth } from '../providers/FakeAuthProvider'
 import styles from './Login.module.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import Button from '../components/Button'
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState('jack@example.com')
   const [password, setPassword] = useState('qwerty')
+  const navigate = useNavigate()
+
+  const { login, isAuthenticated } = useAuth()
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate('/app', { replace: true })
+    },
+    [isAuthenticated, navigate]
+  )
+
+  if (isAuthenticated) return null
 
   return (
     <main className={styles.login}>
       <PageNav />
-      <form className={styles.form}>
+      <form
+        className={styles.form}
+        onSubmit={e => {
+          e.preventDefault()
+          if (email && password) login(email, password)
+        }}
+      >
         <div className={styles.row}>
           <label htmlFor="email">Email address</label>
           <input type="email" id="email" onChange={e => setEmail(e.target.value)} value={email} />
@@ -22,7 +43,7 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary">Login</Button>
         </div>
       </form>
     </main>
